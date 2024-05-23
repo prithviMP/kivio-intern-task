2cab82d4744f9c181c73bc7c817e62b194ef0ae5
kauxp <paramjeetkaur4326@gmail.com>
2024-05-23 16:36:16 +0530

    Added env variables

--- src/zoho/GenerateToken.js
+++ src/zoho/GenerateToken.js
@@ -5,10 +5,10 @@ export default async function AccessToken() {
         url: 'https://accounts.zoho.in/oauth/v2/token',
         method: 'post',
         params: {
-            refresh_token: '',
-            grant_type: '',
-            client_id: '',
-            client_secret: '',
+            refresh_token: process.env.ZOHO_REFRESH_TOKEN,
+            grant_type: process.env.ZOHO_GRANT_TYPE,
+            client_id: process.env.ZOHO_CLIENT_ID,
+            client_secret: process.env.ZOHO_CLIENT_SECRET,
         },
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
